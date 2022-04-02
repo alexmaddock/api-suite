13c7cea348a8683a63925b94bfedeeb8719c9410
Alex Maddock <alex.maddock@fusionsport.com>
2022-04-02 14:39:20 +1000

    some more detail on response body

--- test/test.js
+++ test/test.js
@@ -1,12 +1,11 @@
-// var assert = require('assert');
 import assert from 'assert';
 import jwt from '../http-client/api-client.js';
-import { axio } from '../http-client/axios-client.js';
 import expect from 'expect.js';
 
 describe('GET /stackoverflow', function () {
     it('search term returns something', async function () {
       let outcome = await jwt().then((response) => response);
+      console.log(JSON.stringify(await outcome.json(), null, 2))
       expect(outcome.status).to.eql(200);
       expect(outcome.status).to.be.a('string');
       expect(outcome.statusText).to.be.a('string');
